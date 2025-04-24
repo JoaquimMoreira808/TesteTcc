8a26a30a411b9b1efb8f55f6a8f90194ec593bed
JoaquiMoreira808 <joaquimfernando808@gmail.com>
2025-04-24 01:08:16 +0100

    Adicionei as funções de cadastro

--- funcs.py
+++ funcs.py
@@ -1,4 +1,21 @@
-def CadastrarOrientador(nome:str):
-    Orientador = {
-        "nome:[]"
-    }
+Professores = []
+
+def cadastrar_professor(nome:str):
+    Professor = {"nome":nome, "alunos": []}
+    Professores.append(Professor)
+    print(f"Professor {nome} cadastrado com sucesso!")
+
+cadastrar_professor("Joaquim")
+
+Alunos = []
+def CadastrarAlunos(nome:str, matricula:int,orientador:str):
+    Aluno = {"nome":nome, "matricula":matricula, "orientador":orientador, "entregas": []}
+    Alunos.append(Aluno)
+    print(f"Professor {nome} cadastrado com sucesso!")
+
+
+CadastrarAlunos("Lucas", 12345, "Camila")
+CadastrarAlunos("Isabela", 54321, "Joaquim")
+
+
+
